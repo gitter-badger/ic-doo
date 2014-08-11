e071880db5c01503c2ec80f42b5bf7e4dfd19cd4
Walid Ghazzo <walidghazzo@gmail.com>
2014-08-12 01:56:55 +0200

    initial commit

---
+++ server/config/seed.js
@@ -0,0 +1,40 @@
+/**
+ * Populate DB with sample data on server start
+ * to disable, edit config/environment/index.js, and set `seedDB: false`
+ */
+
+'use strict';
+
+var Thing = require('../api/thing/thing.model');
+var User = require('../api/user/user.model');
+
+Thing.find({}).remove(function() {
+  Thing.create({
+    name : 'People',
+    info : 'We are a people driven business – employees, clients and partners are equally vital to our success. We foster respect and trust in everything we do.'
+  }, {
+    name : 'Passion',
+    info : 'We are passionate about shaping the future, we seek new opportunities and we believe in what we do.'
+  }, {
+    name : 'Performance',
+    info : 'We want to be the best in everything we do, we strive to be leaders. We do things right first time and on time.'
+  });
+});
+
+User.find({}).remove(function() {
+  User.create({
+    provider: 'local',
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+  }, {
+    provider: 'local',
+    role: 'admin',
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin'
+  }, function() {
+      console.log('finished populating users');
+    }
+  );
+});
